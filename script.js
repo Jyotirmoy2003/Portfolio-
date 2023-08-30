7e9031ff556ccf50d80a9c9d8ff8040c028899f8
Jyotirmoy2003 <jyotialu78@gmail.com>
2023-08-31 00:44:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+
+
+//smooth scroll
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
